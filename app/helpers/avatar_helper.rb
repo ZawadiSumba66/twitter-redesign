2d4b403ed63cdf173785fdfd6fff7a636b67b6ee
ZawadiSumba66 <rosesumba98@gmail.com>
2021-03-22 13:01:42 +0300

    Add new features to top and left nav

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -3,6 +3,6 @@ module AvatarHelper
     encoded_name = ERB::Util.url_encode(user.fullname)
     src = "https://ui-avatars.com/api/?name=#{encoded_name}&format=svg&background=DBEAFE"
 
-    content_tag(:img, '', src: src, alt: user.fullname, class: 'rounded-circle px-2')
+    content_tag(:img, '', src: src, alt: user.fullname, class: 'rounded-circle')
   end
 end
