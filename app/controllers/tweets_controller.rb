3f667003f8ce2e959adf1def23a682befeeb17ca
ZawadiSumba66 <rosesumba98@gmail.com>
2021-09-13 19:39:48 +0300

    Add delete and edit logic to tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,6 +20,26 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit 
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to root_path, notice: "Tweet updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def tweet_params
